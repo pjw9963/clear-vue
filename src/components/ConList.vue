a8941ba41c77941e5d577db8ccac8dc34017a675
Patrick Wilbur <pjw9963@rit.edu>
2020-05-27 20:09:32 -0400

    inital commit

---
+++ src/components/ConList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="background" >
+        <h2>Cons</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ConList',
+        props: {
+
+        }
+    }
+</script>
+
+<style scoped>
+  #background {
+    background-color: #d68c86;
+    width: 50%;
+    text-align: center;
+    min-height: 100vh;
+  }
+</style>
---
+++ src/components/ProList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="background">
+    <h2>Pros</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProList',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #background {
+    background-color: #86d68c;
+    width: 50%;
+    text-align: center;
+    min-height: 100vh;
+  }
+</style>
