98a91cc9e23ee19248d23264e2e7136750a535c5
Patrick Wilbur <pjw9963@rit.edu>
2020-05-27 21:44:26 -0400

    added dynamic bullet points to pros and cons list

--- src/components/ConList.vue
+++ src/components/ConList.vue
@@ -1,16 +1,46 @@
 <template>
     <div id="background" >
         <h2>Cons</h2>
+        <ul>      
+          <li v-for="item in items" :key="item.id" >
+            <input type="text" v-model="item.message" />
+          </li>              
+        </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'ConList',
-        props: {
+  let data = {
+    items: [
+      {
+        message: '',
+        id: 0
+      }      
+    ]
+  }
 
-        }
-    }
+  export default {
+    name: 'ConList',
+    data: function() {
+      return data;
+    },
+    props: {
+
+    },
+    watch: {
+      items: {
+        handler: function (val) {
+          if (val[val.length - 1].message.length > 0 ) {
+            this.items.push({ message: '', id: val.length })
+          } 
+          if (val[val.length - 1].message.length === 0 && val.length - 2 >= 0 && val[val.length - 2].message.length === 0){
+            this.items.pop();
+          }
+        },
+        deep: true
+      }
+    }      
+  }
 </script>
 
 <style scoped>
@@ -20,4 +50,9 @@
     text-align: center;
     min-height: 100vh;
   }
+  input {
+    outline:none;
+    border: none;
+    background-color: transparent;
+  }  
 </style>
--- src/components/ProList.vue
+++ src/components/ProList.vue
@@ -1,16 +1,47 @@
 <template>
   <div id="background">
     <h2>Pros</h2>
+    <ul>  
+      <li v-for="item in items" :key="item.id" >
+        <input type="text" v-model="item.message" />
+      </li>
+    </ul>    
   </div>
 </template>
 
 <script>
 
+let data = {
+  items: [
+    {
+      message: '',
+      id: 0
+    }
+  ]
+}
+
 export default {
   name: 'ProList',
+  data: function() {
+    return data;
+  },
   props: {
 
+  },
+  watch: {
+    items: {
+      handler: function (val) {
+        if (val[val.length - 1].message.length > 0 ) {
+          this.items.push({ message: '', id: val.length })
+        } 
+        if (val[val.length - 1].message.length === 0 && val.length - 2 >= 0 && val[val.length - 2].message.length === 0){
+          this.items.pop();
+        }
+      },
+      deep: true
+    }
   }
+
 }
 </script>
 
@@ -21,5 +52,10 @@ export default {
     width: 50%;
     text-align: center;
     min-height: 100vh;
+  }
+  input {
+    outline:none;
+    border: none;
+    background-color: transparent;
   }
 </style>
